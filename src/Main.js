e4eb13246349157ac3300b2bffaa993f9cce470c
stanislav-deviatiarov <stasd2008kis@gmail.com>
2024-10-12 21:53:59 +0300

    Implemented displaying of resting players. Reworked UI for mixing results.

--- src/Main.js
+++ src/Main.js
@@ -69,6 +69,14 @@ function DefaultPage() {
     let usedPlayerIndexes = [];
     let currentCourtIndex = 0;
 
+    let restingPlayers = [];
+
+    if (activePlayersCount % 2 !== 0) {
+      let randomPlayerIndex = getRandomInt(0, activePlayersCount - 1);
+      restingPlayers.push(activePlayers[randomPlayerIndex].name);
+      usedPlayerIndexes.push(randomPlayerIndex);
+    }
+
     while (activePlayersCount - usedPlayerIndexes.length > 0) {
       let newIndex = getRandomInt(0, activePlayersCount - 1);
       if (usedPlayerIndexes.find(i => i === newIndex) === undefined) {
@@ -89,11 +97,24 @@ function DefaultPage() {
     for (let court of activeCourts) {
       if (court.players.length > 0) {
         let courtPlayers = [];
+        let firstItem = true;
         for (let player of court.players) {
-          courtPlayers.push(<li>{player}</li>);
+          courtPlayers.push(<li className={firstItem ? 'first' : 'second'}>{player}</li>);
+          firstItem = !firstItem;
         }
         mixedCourts.push(<div className='mix-block-result'><h1>{court.name}</h1><ul>{courtPlayers}</ul></div>)
       }
+    }
+
+    if (restingPlayers.length !== 0 || activePlayersCount > usedPlayerIndexes.length) {
+      restingPlayers = restingPlayers.concat(activePlayers.filter(ap => !usedPlayerIndexes.includes(ap.id)).map(rp => rp.name));
+      let resting = [];
+      for (let player of restingPlayers) {
+        resting.push(<li>{player}</li>);
+      }
+      mixedCourts.push(
+      <div className='mix-block-result'><h1>{resting.length > 1 ? 'Відпочивають' : 'Відпочиває' }</h1>
+      <ul className='resting'>{resting}</ul></div>);
     }
 
     setMixed(mixedCourts);
@@ -104,7 +125,8 @@ function DefaultPage() {
   <div className='players-back'>
     <h1>Список гравців</h1>
     <div className='addNewPlayer'>
-      <input type='text' maxLength={12} onChange={e => setPlayerName(e.target.value)} value={playerName} placeholder='До 12 символів' />
+      <input type='text' maxLength={12} onChange={e => setPlayerName(e.target.value)}
+      value={playerName} placeholder='До 12 символів' />
       <input type='button' value='Додати' style={{'margin-left': '5px'}} onClick={handleAddNewPlayer} />
     </div>
     <ItemList itemClass="players" listElements={players} onToggle={handlePlayerToggle} onDelete={handlePlayerDelete} />
